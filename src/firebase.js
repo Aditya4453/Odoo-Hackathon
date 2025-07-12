1f1e40dd0da3630d7c4dca0c79446136653194f6
Aditya <modaniaditya06@gmail.com>
2025-07-12 10:46:54 +0530

    FIREBASE ADDED

---
+++ src/firebase.js
@@ -0,0 +1,17 @@
+// src/firebase.js
+import { initializeApp } from "firebase/app";
+import { getAuth } from "firebase/auth";
+import { getFirestore } from "firebase/firestore";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyBoFW8rzt7s0xq_0ZF4wQlG_QFLyXIvgKk",
+  authDomain: "skillswap-30254.firebaseapp.com",
+  projectId: "skillswap-30254",
+  storageBucket: "skillswap-30254.firebasestorage.app",
+  messagingSenderId: "147317696119",
+  appId: "1:147317696119:web:f5500089a6532d089b8872"
+};
+
+const app = initializeApp(firebaseConfig);
+export const auth = getAuth(app);
+export const db = getFirestore(app);
